3877829757480ebe09581cb14ca13eb947fca6b7
Mohammed Mushahid Ali <alimushahid24@gmail.com>
2023-08-17 15:23:19 +0530

    CARDS -> DONE

---
+++ client/src/components/cards/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import CardIcon from '../../assets/logo.png'
+import './index.css'
+
+export default function index({element}) {
+    return (
+        <div className="cards-main">
+            <center><img src={CardIcon} width={100} height={100} style={{ borderRadius: '50px' }} /></center><br />
+            <div className="cards-inner-main">
+                <h4 className="d-flex flex-wrap">
+                    <b>{element.title}</b>
+                </h4><br />
+                <div className="d-flex align-items-center justify-content-between">
+                    <div className="d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                        </svg>
+                        <p>&nbsp;{element.months} Months</p>
+                    </div>
+                    <div className="d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-diagram-3" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zM8.5 5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1zM0 11.5A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
+                        </svg>
+                        <p>&nbsp;{element.courses} Courses</p>
+                    </div>
+                </div><hr />
+                <div className="d-flex align-items-center">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16">
+                            <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z" />
+                            <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
+                        </svg>
+                    </div>
+                    <div>&nbsp;&nbsp;
+                        {element.summary}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -1,9 +1,34 @@
+import Cards from '../cards'
 import './index.css'
 
 export default function index() {
+
+    const cardData = [
+        {
+            title: "MERN Course",
+            months: "11",
+            courses: "12",
+            summary: "Simplilearn Career Bootcamp"
+        },
+        {
+            title: "HTML Course",
+            months: "3",
+            courses: "3",
+            summary: "Simplilearn Career Bootcamp"
+        },
+    ];
+
     return (
         <div className="home-main">
             HOME
+            <br />
+            <div>
+                {
+                    cardData.map(element => <div className="d-flex flex-row flex-wrap justify-content-center">
+                        <Cards element={element} />
+                    </div>)
+                }
+            </div>
         </div>
     );
 }
